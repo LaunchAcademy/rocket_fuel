195e8600de39b25a6814819aa750a93797f34e95
Dan Pickett <dan.pickett@launchware.com>
2014-10-17 16:51:57 -0400

    s/rvm/rbenv

--- spec/rocket_fuel/precheck/rbenv_check_spec.rb
+++ spec/rocket_fuel/precheck/rbenv_check_spec.rb
@@ -10,25 +10,25 @@ describe RocketFuel::Precheck::RbenvCheck do
   let(:global_path) { RocketFuel::Precheck::RbenvCheck.global_path  }
   let(:check) { RocketFuel::Precheck::RbenvCheck.new }
 
-  it 'is not ok if rvm is found in the users home directory' do
+  it 'is not ok if rbenv is found in the users home directory' do
     FileUtils.mkdir_p(home_path)
     expect(check).to_not be_ok
   end
 
-  it 'is not ok if rvm is found in the global directory' do
+  it 'is not ok if rm is found in the global directory' do
     FileUtils.mkdir_p(global_path)
     expect(check).to_not be_ok
   end
 
-  it 'is ok if rvm is not found or installed' do
+  it 'is ok if rbenv is not found or installed' do
     expect(check).to be_ok
   end
 
-  it 'has a success message if rvm is not found' do
+  it 'has a success message if rbenv is not found' do
     expect(check.message).to match(/not found/i)
   end
 
-  it 'has a failure message if rvm is found' do
+  it 'has a failure message if rbenv is found' do
     FileUtils.mkdir_p(home_path)
     expect(check.message).to match(/found/i)
   end
